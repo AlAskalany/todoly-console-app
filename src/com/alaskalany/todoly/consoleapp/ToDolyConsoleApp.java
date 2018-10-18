6b4dd35cbbead602b1a518560a2f51093191ec2c
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-18 08:15:39 +0200

    Initial commit

---
+++ src/com/alaskalany/todoly/consoleapp/ToDolyConsoleApp.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018. Ahmed AlAskalany (ahmed.alaskalany@gmail.com).
+ */
+package com.alaskalany.todoly.consoleapp;
+
+public class ToDolyConsoleApp {
+
+    public static void main(String[] args) {
+        System.out.println("ToDoly Console Application");
+    }
+}
---
+++ test/com/alaskalany/todoly/consoleapp/ToDolyConsoleAppTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018. Ahmed AlAskalany (ahmed.alaskalany@gmail.com).
+ */
+
+package com.alaskalany.todoly.consoleapp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDolyConsoleAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
